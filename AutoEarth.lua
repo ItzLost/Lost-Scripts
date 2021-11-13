557eba8fbdedc71c15fd99b430ee5ee004a4f612
Katsu-Toshiro <93594637+Katsu-Toshiro@users.noreply.github.com>
2021-11-13 09:54:27 +0100

    Create AutoEarth.lua

---
+++ AutoEarth.lua
@@ -0,0 +1,346 @@
+game.workspace.FriendlyNPCs["Quest Giver"]:Destroy()
+game.workspace.FriendlyNPCs["Quest Giver"]:Destroy()
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Young Man"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+wait(0.5)
+ChatAdvance:FireServer(
+{
+[1] = "Yes"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Suitcase"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Bulma"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "Yes"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Spaceship"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "No"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+--/////////////////////////////////////////////////////
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Trunks [Future]"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "Yes"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["TimeMachine"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "No"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Quest Giver"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "Yes"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["NamekianShip"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "No"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Elder Kai"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "Yes"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(2.5)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+workspace.FriendlyNPCs["Korin"]
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
+wait (0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "DRINK"
+}
+)
+wait(0.5)
+local ChatAdvance =
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+{
+[1] = "k"
+}
+)
