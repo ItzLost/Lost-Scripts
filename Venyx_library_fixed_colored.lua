31e3bfe8cb9568e1211e6a28fc4c38cf1531eeea
Katsu-Toshiro <93594637+Katsu-Toshiro@users.noreply.github.com>
2022-06-29 11:41:20 +0200

    Update Venyx_library_fixed_colored.lua

--- Venyx_library_fixed_colored.lua
+++ Venyx_library_fixed_colored.lua
@@ -65,12 +65,12 @@ do
 	}
 
 	local THEME = {
-		Background = Color3.fromRGB(24, 24, 24),
-		Glow = Color3.fromRGB(0, 0, 0),
-		Accent = Color3.fromRGB(10, 10, 10),
-		LightContrast = Color3.fromRGB(20, 20, 20),
-		DarkContrast = Color3.fromRGB(34, 34, 34),
-		TextColor = Color3.fromRGB(255, 255, 255)
+		Background = Color3.fromRGB(24, 24, 24), 
+		Glow = Color3.fromRGB(109, 0, 255), 
+		Accent = Color3.fromRGB(10, 10, 10), 
+		LightContrast = Color3.fromRGB(20, 20, 20), 
+		DarkContrast = Color3.fromRGB(14, 14, 14),  
+		TextColor = Color3.fromRGB(109, 0, 255)
 	}
 
 	local TAB_INFO = TweenInfo.new(0.5)
