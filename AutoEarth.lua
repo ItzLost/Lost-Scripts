7a6f9c364fc36d20614eb77503cde85ca8ff966d
Lost <93594637+ItzLost@users.noreply.github.com>
2022-07-30 15:08:52 +0200

    Update AutoEarth.lua

--- AutoEarth.lua
+++ AutoEarth.lua
@@ -113,7 +113,6 @@ ChatAdvance:FireServer(
 }
 )
 wait(0.7)
---/////////////////////////////////////////////////////
 local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
 ChatStart:FireServer(
 workspace.FriendlyNPCs["Trunks [Future]"]
@@ -293,54 +292,8 @@ ChatAdvance:FireServer(
 }
 )
 wait(2.5)
-local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
-ChatStart:FireServer(
-workspace.FriendlyNPCs["Korin"]
-)
-wait (0.5)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "k"
-}
-)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "k"
-}
-)
-wait (0.5)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "k"
-}
-)
-wait(0.5)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "k"
-}
-)
-wait (0.5)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "DRINK"
-}
-)
-wait(0.5)
-local ChatAdvance =
-game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
-ChatAdvance:FireServer(
-{
-[1] = "k"
-}
-)
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart:FireServer(game:GetService("Workspace").FriendlyNPCs["Korin"].Chat.Chat)
+wait(.5)
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance:FireServer({[1] = "k"})
+wait(.5)
+game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance:FireServer({[1] = "DRINK"})
